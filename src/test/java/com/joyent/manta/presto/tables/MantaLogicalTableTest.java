54c526c1e76ee4c65b9b9d64171cf8edd3559c7d
Elijah Zupancic <elijah@zupancic.name>
2017-12-21 11:18:11 -0800

    Moved test JSON data set to external JSON data files This change should make it easier to create sample data files when testing serialization of the MantaLogicalTable class.

--- src/test/java/com/joyent/manta/presto/tables/MantaLogicalTableTest.java
+++ src/test/java/com/joyent/manta/presto/tables/MantaLogicalTableTest.java
@@ -17,10 +17,14 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.regex.Pattern;
 
 @Test
 public class MantaLogicalTableTest {
+    private final String basePath = "test-data/logical-table-definition/";
+    private final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
     private Injector injector;
     private ObjectMapper mapper;
 
@@ -32,86 +36,115 @@ public class MantaLogicalTableTest {
     }
 
     public void canSerializeFromJsonWithNullFilters() throws IOException {
-        String input = "{ \"name\":\"logical-table-1\", \"rootPath\":\"/user/stor/json-examples\", \"dataFileType\":\"NDJSON\",  \"directoryFilterRegex\":\"\", \"filterRegex\":\"\" }";
+        final String resourcePath = basePath + "null-filters.json";
 
-        MantaLogicalTable expected = new MantaLogicalTable("logical-table-1",
-                "/user/stor/json-examples",
-                MantaDataFileType.NDJSON);
-        MantaLogicalTable actual = mapper.readValue(input, MantaLogicalTable.class);
-        Assert.assertEquals(expected, actual);
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            MantaLogicalTable expected = new MantaLogicalTable("logical-table-1",
+                    "/user/stor/json-examples",
+                    MantaDataFileType.NDJSON);
+            MantaLogicalTable actual = mapper.readValue(input, MantaLogicalTable.class);
+            Assert.assertEquals(expected, actual);
+        }
     }
 
     public void canSerializeFromJsonWithDirectoryFilter() throws IOException {
-        String input = "{ \"name\":\"logical-table-1\", \"rootPath\":\"/user/stor/json-examples\", \"dataFileType\":\"NDJSON\",  \"directoryFilterRegex\":\"^.*dir2\\/.*\\\\.json$\", \"filterRegex\":\"\" }";
+        final String resourcePath = basePath + "with-directory-filters.json";
 
-        MantaLogicalTable expected = new MantaLogicalTable("logical-table-1",
-                "/user/stor/json-examples",
-                MantaDataFileType.NDJSON,
-                Pattern.compile("^.*dir2/.*\\.json$"), null);
-        MantaLogicalTable actual = mapper.readValue(input, MantaLogicalTable.class);
-        Assert.assertEquals(expected, actual);
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            MantaLogicalTable expected = new MantaLogicalTable("logical-table-1",
+                    "/user/stor/json-examples",
+                    MantaDataFileType.NDJSON,
+                    Pattern.compile("^.*dir2/.*\\.json$"), null);
+            MantaLogicalTable actual = mapper.readValue(input, MantaLogicalTable.class);
+            Assert.assertEquals(expected, actual);
+        }
     }
 
     public void canSerializeFromJsonWithFilter() throws IOException {
-        String input = "{ \"name\":\"logical-table-1\", \"rootPath\":\"/user/stor/json-examples\", \"dataFileType\":\"NDJSON\", \"directoryFilterRegex\":\"\", \"filterRegex\":\"^.*\\\\.json$\" }";
+        final String resourcePath = basePath + "with-file-filters.json";
 
-        MantaLogicalTable expected = new MantaLogicalTable("logical-table-1",
-                "/user/stor/json-examples",
-                MantaDataFileType.NDJSON,
-                null, Pattern.compile("^.*\\.json$"));
-        MantaLogicalTable actual = mapper.readValue(input, MantaLogicalTable.class);
-        Assert.assertEquals(expected, actual);
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            MantaLogicalTable expected = new MantaLogicalTable("logical-table-1",
+                    "/user/stor/json-examples",
+                    MantaDataFileType.NDJSON,
+                    null, Pattern.compile("^.*\\.json$"));
+            MantaLogicalTable actual = mapper.readValue(input, MantaLogicalTable.class);
+            Assert.assertEquals(expected, actual);
+        }
     }
 
     public void canSerializeFromJsonWithMissingFilters() throws IOException {
-        String input = "{ \"name\":\"logical-table-1\", \"rootPath\":\"/user/stor/json-examples\", \"dataFileType\":\"NDJSON\" }";
+        final String resourcePath = basePath + "missing-filters.json";
 
-        MantaLogicalTable expected = new MantaLogicalTable("logical-table-1",
-                "/user/stor/json-examples",
-                MantaDataFileType.NDJSON);
-        MantaLogicalTable actual = mapper.readValue(input, MantaLogicalTable.class);
-        Assert.assertEquals(expected, actual);
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            MantaLogicalTable expected = new MantaLogicalTable("logical-table-1",
+                    "/user/stor/json-examples",
+                    MantaDataFileType.NDJSON);
+            MantaLogicalTable actual = mapper.readValue(input, MantaLogicalTable.class);
+            Assert.assertEquals(expected, actual);
+        }
     }
 
     @Test(expectedExceptions = JsonMappingException.class)
     public void cantSerializeFromJsonWithBlankName() throws IOException {
-        String input = "{ \"name\":\"\", \"rootPath\":\"/user/stor/json-examples\", \"dataFileType\":\"json\" }";
-        mapper.readValue(input, MantaLogicalTable.class);
+        final String resourcePath = basePath + "blank-name.json";
+
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            mapper.readValue(input, MantaLogicalTable.class);
+        }
     }
 
     @Test(expectedExceptions = JsonMappingException.class)
     public void cantSerializeFromJsonWithMissingName() throws IOException {
-        String input = "{ \"rootPath\":\"/user/stor/json-examples\", \"dataFileType\":\"json\" }";
-        mapper.readValue(input, MantaLogicalTable.class);
+        final String resourcePath = basePath + "missing-name.json";
+
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            mapper.readValue(input, MantaLogicalTable.class);
+        }
     }
 
     @Test(expectedExceptions = JsonMappingException.class)
     public void cantSerializeFromJsonWithNullName() throws IOException {
-        String input = "{ \"name\":null, \"rootPath\":\"/user/stor/json-examples\", \"dataFileType\":\"json\" }";
-        mapper.readValue(input, MantaLogicalTable.class);
+        final String resourcePath = basePath + "null-name.json";
+
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            mapper.readValue(input, MantaLogicalTable.class);
+        }
     }
 
     @Test(expectedExceptions = JsonMappingException.class)
     public void cantSerializeFromJsonWithBlankRootPath() throws IOException {
-        String input = "{ \"name\":\"logical-table-1\", \"rootPath\":\"\", \"dataFileType\":\"json\" }";
-        mapper.readValue(input, MantaLogicalTable.class);
+        final String resourcePath = basePath + "blank-root-path.json";
+
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            mapper.readValue(input, MantaLogicalTable.class);
+        }
     }
 
     @Test(expectedExceptions = JsonMappingException.class)
     public void cantSerializeFromJsonWithMissingRootPath() throws IOException {
-        String input = "{ \"name\":\"logical-table-1\", \"dataFileType\":\"json\" }";
-        mapper.readValue(input, MantaLogicalTable.class);
+        final String resourcePath = basePath + "missing-root-path.json";
+
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            mapper.readValue(input, MantaLogicalTable.class);
+        }
     }
 
     @Test(expectedExceptions = JsonMappingException.class)
     public void cantSerializeFromJsonWithNullRootPath() throws IOException {
-        String input = "{ \"name\":\"logical-table-1\",  \"rootPath\":null, \"dataFileType\":\"json\" }";
-        mapper.readValue(input, MantaLogicalTable.class);
+        final String resourcePath = basePath + "null-root-path.json";
+
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            mapper.readValue(input, MantaLogicalTable.class);
+        }
     }
 
     @Test(expectedExceptions = JsonMappingException.class)
     public void cantSerializeFromJsonWithBadPattern() throws IOException {
-        String input = "{ \"name\":\"logical-table-1\", \"rootPath\":\"/user/stor/json-examples\", \"dataFileType\":\"json\", \"directoryFilterRegex\":\"**dir2\\/.*\\\\.json$\" }";
-        mapper.readValue(input, MantaLogicalTable.class);
+        final String resourcePath = basePath + "bad-pattern.json";
+
+        try (InputStream input = classLoader.getResourceAsStream(resourcePath)) {
+            mapper.readValue(input, MantaLogicalTable.class);
+        }
     }
 }
